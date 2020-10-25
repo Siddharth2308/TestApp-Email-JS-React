889001997a70c91359ff3213003edf6e3219f414
Siddharth Kulkarni <punesiddharth@gmail.com>
2020-10-25 18:20:10 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import emailjs from 'emailjs-com';
+//import contactForm from './components/contactForm';
+
 
 function App() {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_eezmmjk', 'template_fx0k31k', e.target, 'user_xilh6mX0p8nSY3NkYRfKC')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    e.target.reset();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+      <form onSubmit={sendEmail}>
+        <div className="row pt-5 mx-auto">
+          <div className="col-8 form-group mx-auto">
+            <input type="text" className="form-control" placeholder="Name" name="from_name" />
+          </div>
+          <div className="col-8 form-group pt-2 mx-auto">
+            <input type="email" className="form-control" placeholder="Email Address" name="email" />
+          </div>
+          <div className="col-8 form-group pt-2 mx-auto">
+            <input type="text" className="form-control" placeholder="Subject" name="subject" />
+          </div>
+          <div className="col-8 form-group pt-2 mx-auto">
+            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
+          </div>
+          <div className="col-8 pt-3 mx-auto">
+            <input type="submit" className="btn btn-info" value="Send Message"></input>
+          </div>
+        </div>
+      </form>
+      </div>  
     </div>
   );
 }
